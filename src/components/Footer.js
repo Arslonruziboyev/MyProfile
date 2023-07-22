f8d85686603416957f744e8c946363bbbe231c8c
02arslon <arslonruziboyev@gmail.com>
2023-07-22 19:19:31 +0900

    changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,18 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedinIcon from "@mui/icons-material/LinkedIn";
 import GithubIcon from "@mui/icons-material/GitHub";
+import "../styles/footer.css";
 
 function Footer() {
   return (
     <div className="footer">
+      <div className="socialMedia">
+        <LinkedinIcon />
+        <InstagramIcon />
+        <FacebookIcon />
+
+        <GithubIcon />
+      </div>
       <p>&copy; 2023 Aslan, all rughts reserved</p>
     </div>
   );
