4d0c6719f5a6b580304ce2cd88180ad9c403d047
02arslon <arslonruziboyev@gmail.com>
2023-07-22 19:22:08 +0900

    changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Home() {
-  return <div>1</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h3>Hi, my name is Arslon Ruziboev</h3>
+        <div className="prompt">
+          <p>A software developer with a passion for learing and creating</p>
+        </div>
+      </div>
+      <div className="skills"></div>
+    </div>
+  );
 }
 
 export default Home;
