f9095698c69f2ec7f19f0e6ed45a782097d7a011
02arslon <arslonruziboyev@gmail.com>
2023-07-22 19:02:32 +0900

    changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedinIcon from "@mui/icons-material/LinkedIn";
+import GithubIcon from "@mui/icons-material/GitHub";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <p>&copy; 2023 Aslan, all rughts reserved</p>
+    </div>
+  );
 }
 
 export default Footer;
