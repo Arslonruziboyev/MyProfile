fd0913930a55004b4adfe621512e0ab17334c5e7
02arslon <arslonruziboyev@gmail.com>
2023-07-23 18:48:42 +0900

    changed

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,9 +4,21 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
 
 function Experience() {
-  return <div>3</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="verticle-timeline-element--education"
+          date="2010-2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
