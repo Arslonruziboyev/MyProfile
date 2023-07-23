2741006979fd91ee1e01c267c2f5215a7fb96253
02arslon <arslonruziboyev@gmail.com>
2023-07-23 18:43:22 +0900

    changed

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,6 +3,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 function Experience() {
   return <div>3</div>;
