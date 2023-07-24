ef8dd97760e0044795a84113241a235699f6d7fa
02arslon <arslonruziboyev@gmail.com>
2023-07-24 19:08:35 +0900

    changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,27 @@
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import LinkedinIcon from "@mui/icons-material/LinkedIn";
+import LinkedinIcon from "@mui/icons-material/te";
 import GithubIcon from "@mui/icons-material/GitHub";
+import TelegramIcon from "@mui/icons-material/Telegram";
 import "../styles/footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <LinkedinIcon />
+        <a href="https://www.linkedin.com/in/arslon-ruziboev-a92385198/">
+          <LinkedinIcon />
+        </a>
+        <a href="https://github.com/Arslonruziboyev">
+          <GithubIcon />
+        </a>
+        <a href="https://t.me/arslon_ruziboev">
+          <TelegramIcon />
+        </a>
+
         <InstagramIcon />
         <FacebookIcon />
-
-        <GithubIcon />
       </div>
       <p>&copy; 2023 Aslan, all rights reserved</p>
     </div>
