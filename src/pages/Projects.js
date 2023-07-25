0d4a8712bcbad919728d3cc0e16ea11bdb9bbf0e
02arslon <arslonruziboyev@gmail.com>
2023-07-25 18:31:54 +0900

    changed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
+import Img1 from "../assets/proj2.jpg";
+import Img2 from "../assets/proj5.jpg";
 
 function Projects() {
   return (
     <div className="projects">
       <h1>My own projects</h1>
       <div className="projectLists">
-        <ProjectItem />
+        <ProjectItem name="Social Media Website" image={Img1} />
+        <ProjectItem name="Social Media Website" image={Img2} />
       </div>
     </div>
   );
