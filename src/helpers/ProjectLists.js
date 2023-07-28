a17b63dbc6894276e07ab67330045ea66cbfcd56
02arslon <arslonruziboyev@gmail.com>
2023-07-28 17:29:23 +0900

    changed

--- src/helpers/ProjectLists.js
+++ src/helpers/ProjectLists.js
@@ -1,20 +1,16 @@
-import Img1 from "../assets/proj2.jpg";
+import GPT1 from "../assets/GPT.JPG";
 import Img2 from "../assets/proj5.jpg";
 
 export const ProjectLists = [
   {
-    name: "Socail Website",
-    image: Img1,
-    skills: "Html, Css",
+    name: "GPT Modren Website ",
+    image: GPT1,
+    skills: "HTML, CSS, JAVASCRIPT, REACT, Reponsive Design",
+    linkToWebsite: "https://awsomegpt-byaslan.netlify.app/",
   },
   {
     name: "Neme Website",
     image: Img2,
-    skills: "Html, Css",
-  },
-  {
-    name: "New Website",
-    image: Img1,
     skills: "Html, Css",
   },
 ];
